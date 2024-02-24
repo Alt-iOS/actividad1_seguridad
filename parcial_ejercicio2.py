435f5312fbcdc4bbe5c7c8819b794b0a8e550efe
victor <vjbsh@hotmail.com>
2024-02-23 21:30:54 -0600

    finished pt2

--- parcial_ejercicio2.py
+++ parcial_ejercicio2.py
@@ -31,17 +31,23 @@ signature_bytes = signature.to_bytes((signature.bit_length() + 7) // 8, byteorde
 with open("NDA.pdf", "ab") as f:
     f.write(signature_bytes)
 
-# restamos la firma del archivo
-with open("NDA.pdf", "rb") as f:
-    pdf_bytes = f.read()[:-256]
-
 # checamos los ultimos 256 bytes
 signature_bytes_from_pdf_AC = read_last_bytes("NDA.pdf", 256)
 #los convertimos a int
 signature_int_from_pdf_AC = int.from_bytes(signature_bytes_from_pdf_AC, byteorder='big')
+
+# restamos la firma del archivo
+with open("NDA.pdf", "rb") as f:
+    pdf_bytes_AC = f.read()[:-256]
+    pdf_hash_AC = int.from_bytes(hashlib.sha256(pdf_bytes_AC).digest(), "big")
+
 # Verificación por AC con la publica de Alice
-pdf_hash_verif = pow(signature_int_from_pdf_AC, e, nA)
-print("Firma verificada por AC:", pdf_hash_verif == pdf_hash)
+sig_verif_AC = pow(signature_int_from_pdf_AC, e, nA)
+print("Firma verificada por AC:", sig_verif_AC == pdf_hash_AC)
+
+#una vez esta verificado, removemos la firma del archivo
+with open("NDA.pdf", "wb") as f:
+    f.write(pdf_bytes_AC)
 
 # Generación de llaves para AC
 pAC = getPrime(bits, randfunc=Crypto.Random.get_random_bytes)
@@ -52,15 +58,24 @@ eAC = 65537
 dAC = inverse(eAC, phiAC)
 
 # Firma de AC con la publica de AC
-signature_ac = pow(pdf_hash, dAC, nAC)
+signature_ac = pow(pdf_hash_AC, dAC, nAC)
 
 # Agregamos la firma de AC al final del archivo
 signature_ac_bytes = signature_ac.to_bytes(
-    (signature_ac.bit_length() + 7) // 8, byteorder="big"
-)
+    (signature_ac.bit_length() + 7) // 8, byteorder="big")
 with open("NDA.pdf", "ab") as f:
     f.write(signature_ac_bytes)
 
 # Verificación por Bob
+# checamos los ultimos 256 bytes
+signature_bytes_from_pdf_BOB = read_last_bytes("NDA.pdf", 256)
+#los convertimos a int
+signature_int_from_pdf_BOB = int.from_bytes(signature_bytes_from_pdf_BOB, byteorder='big')
+
+# restamos la firma del archivo
+with open("NDA.pdf", "rb") as f:
+    pdf_bytes_BOB = f.read()[:-256]
+    pdf_hash_BOB = int.from_bytes(hashlib.sha256(pdf_bytes_BOB).digest(), "big")
+
 pdf_hash_verif_bob = pow(signature_ac, eAC, nAC)
-print("Firma verificada por Bob:", pdf_hash_verif_bob == pdf_hash)
+print("Firma verificada por Bob:", pdf_hash_verif_bob == pdf_hash_AC)
